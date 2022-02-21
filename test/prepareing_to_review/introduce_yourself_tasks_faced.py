874a6015a50989a7f1a47f5070ac2c0d7a0f7779
n-varvonets <nickolay.varvonets@gmail.com>
2022-02-21 11:13:31 +0200

    updated of preparing to review

--- test/prepareing_to_review/introduce_yourself_tasks_faced.py
+++ test/prepareing_to_review/introduce_yourself_tasks_faced.py
@@ -1,10 +1,34 @@
 # 1) Представь себя:
 # я люблю в свободное время постигать такие как itdvn  и прорабатывать книгу марка лутца пайтон 3 издание, смотрю хабр
 
-
 # но если вкратце, то:
 # 1) проанализировать всевозможные источники в интеренете, которые имеют данные о стартапах и по возможности заскрапить
 # их, а если не получится то через api получить как можно больше данных
-# 2)их запарсить с четкой структурой для части меш.лернинг и фронтенда и внести в бд
-# 3)создать работающий api  с нашой бд с помощью no-beckend  фрейморка pgREST и использовать traefik для проксирования запросов
-# 4) как результат вот (http://dev-postgrest.ledgity.com/dealroom) работающая  апи,
+# 2) их запарсить с четкой структурой для части меш.лернинг и фронтенда и внести в бд
+# 3)мсоздать работающий api  с нашой бд с помощью no-beckend  фрейморка pgREST и использовать traefik для проксирования запросов
+
+
+"""me"""
+# I was born and raised in Odessa
+# I've been interested in football and computer games since I was a kid.
+# Even I have my own achievements in this sport (two-time Ukrainian Cup winner and three-time Ukranian champion in my age group...was a member of the national team )
+"""education"""
+#  I received my bachelor's degree in speciality of petroleum engineering in Odessa National Academy of Food
+#  Tech and the master's degree by the same speciality I got in National University of Oil and Gas in Ivano-Frankivsk.
+#  I was working by my speciality approximately for a year and half. I was disappointed because of being an honest
+#  engenier by your own efforts and knowleges you will not achieve a higher postion.
+"""why i go into IT"""
+# And because I've liked technology since I was a kid and my older brother works as a prigrammer,
+# I decided to try my hand at it under my older brother's mentorship
+#  and I have been working as a programmer for about a year and a half now and I am happy that I chose this path
+"""current work and why I want to quiet"""
+# well, at the head of it all is the material basis
+# One of the reasons why I want to change jobs is that I am given to work with everything and not with what I want.
+# So for me, the technology stack that I will be working with is important. I want to get real experience working with:
+#     - DjangoREST, Celery;
+#     - Docker, CI/CD.
+"""expectations from team"""
+# it's important that the team has to be united one general point and also there is a good communcation
+# and support of each other as one team
+"""salary"""
+# depends on the conditions of the relocation, but in the amount of one and a half thousand
