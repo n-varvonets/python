39a6a06c67ca5a6825dcb3b6d0d8ec2c87a4bc97
Nickolay <nickolay.varvonets@gmail.com>
2022-05-01 22:09:11 +0300

    processed gevent block

--- Advanced/3. АСИНХРОННОЕ И МНОГОПОТОЧНОЕ ПРОГРАММИРОВАНИЕ/practice/_async/_gevent/example2_event.py
+++ Advanced/3. АСИНХРОННОЕ И МНОГОПОТОЧНОЕ ПРОГРАММИРОВАНИЕ/practice/_async/_gevent/example2_event.py
@@ -1,6 +1,13 @@
+import time
+
 import gevent
 from gevent.event import Event
 
+
+"""здесь есть две асинхронные задачи:
+- 1. waiter() и emitter() - их синхронизируем через обьект ивент Event()
+- 2 endless() - базис vent_loop, но только spawn(гринлет)...
+"""
 
 def waiter():
     print('I am waiting for event')
@@ -17,10 +24,12 @@ def emitter():
     event.set()
     print('I kill endless task!')
     # останавливаем бесконечный greenlet принудилеьно
-    endless_task.kill()
+    # endless_task.kill()
 
 
 def endless():
+    """это совершенно другая задача, ивент_лупа, которая выполнятся
+    независимо от двух выше асинхронных через ивент обьект"""
     while True:
         print('Endless Task will be working forever!')
         gevent.sleep(2)
@@ -29,12 +38,15 @@ def endless():
 # создаем Event, который импортировали из модуля gevent.event
 event = Event()
 
-endless_task = gevent.spawn(endless)
-jobs = [
-    gevent.spawn(emitter),
-    gevent.spawn(waiter),
-    endless_task,
-]
+endless_task = gevent.spawn(endless)  # получаем обьект, без запуска
+# проверяем что нет печати >>> 'Endless Task will be working forever!'
+print('--1--')
+time.sleep(2)
 
+jobs = [
+    gevent.spawn(emitter),  # эта функция(гринлет-корутина) 1 раз выполнится
+    gevent.spawn(waiter),  #  эта функция(гринлет-корутина) 1 раз выполнится
+    endless_task,  #  эта функция(гринлет-корутина) будет постоянно выполняться, пока не kill её
+]
 
 gevent.wait(jobs)
