10850ccf07d1c2e915bdb19c441803af84ad2545
Nickolay <nickolay.varvonets@gmail.com>
2022-05-24 20:35:57 +0300

    added gutenberg books

---
+++ inst_api_and_gutenberg_ebooks/gutenberg_ebooks/unzip_files.py
@@ -0,0 +1,13 @@
+import zipfile
+from os import listdir
+from os.path import isfile, join
+
+DIRECTORY_TO_EXTRACT_FROM = '/home/nick/PycharmProjects/gutenberg_ebooks/data/output/zip'
+DIRECTORY_TO_EXTRACT_TO = '/home/nick/PycharmProjects/gutenberg_ebooks/data/output/txt'
+
+files = [f for f in listdir(DIRECTORY_TO_EXTRACT_FROM) if isfile(join(DIRECTORY_TO_EXTRACT_FROM, f))]
+
+for file in files:
+    dir_file_extract_from = '/'.join((DIRECTORY_TO_EXTRACT_FROM, file))
+    with zipfile.ZipFile(dir_file_extract_from, 'r') as zip_ref:
+        zip_ref.extractall(DIRECTORY_TO_EXTRACT_TO)
