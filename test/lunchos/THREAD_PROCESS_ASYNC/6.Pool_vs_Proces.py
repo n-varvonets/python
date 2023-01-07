ed7d461fa9ac574b251c8f4137dcfdb127d5bec6
n-varvonets <nickolay.varvonets@gmail.com>
2023-01-07 21:46:47 +0200

    luchanos Process vs Pool from multiprocess

---
+++ test/lunchos/THREAD_PROCESS_ASYNC/6.Pool_vs_Proces.py
@@ -0,0 +1,77 @@
+import multiprocessing
+import os
+from random import randint
+from datetime import datetime
+from multiprocessing import Process, Pool
+
+with open("6_part_data/file.txt", "w") as f_o:
+    """просто запишем файл с данньіми, которьіе потом будем перебирать в разньіх процах"""
+    for _ in range(10_000_000):
+        f_o.write(f"{randint(0, 9)}\n")
+
+
+def foo(*args):
+    """
+    функция будет читать файл с масивом данньіх(json) с условно сложньім  CPU вьічислением внутри
+    :return:
+    """
+    result = 0
+    with open("6_part_data/file.txt", "r") as f_o:
+        for s in f_o:
+            # our hard perfomance logic
+            # pass  # for same result. below - for unknown prediction
+            result += randint(0, int(s))  # for example, we will sum some value
+    print(os.getpid(), result)
+
+
+# 1) простой запуск - 0:00:33.795758
+# if __name__ == "__main__":
+#     """
+#     два раза вьізовем одну и ту же функцию
+#     ожидаемьій еффект: функции поседовательно отработают в одном процессе
+#     """
+#     start = datetime.now()
+#     foo()
+#     foo()
+#     print(datetime.now() - start)
+
+
+# 2.1) запуск функции из обертки Process c двумя паралельно запущенньіми процесами и главньій дожидается конца их работьі- 0:00:38.443845
+# if __name__ == "__main__":
+#     """ожидаем еффект:
+#     одновременно функции отрабатаю и віьдадут результат, тем самьіь ускорив работу"""
+#     start = datetime.now()
+#     p = Process(target=foo)  # передаем обьект функции, а не вьізіваем ее
+#     p_1 = Process(target=foo)
+#     p.start()
+#     p_1.start()
+#     p.join()  # не пойдет дальше пока до тех пор, пока p не заверщится
+#     print("p заверщился")
+#     p_1.join()  # не пойдет дальше пока до тех пор, пока p_1 не заверщится
+#     print("p_1 заверщился")
+#     print(f"final time:", datetime.now() - start)
+
+# 2.2) тот же саьій запуск, НО + рефакторинг лайф  0:00:38.159275
+# if __name__ == "__main__":
+#     """ожидаем еффект:
+#     одновременно функции отрабатаю и віьдадут результат, тем самьіь ускорив работу"""
+#     start = datetime.now()
+#     p_list = [Process(target=foo) for _ in range(2)] # передаем обьект функции, а не вьізіваем ее
+#     for p in p_list:
+#         p.start()
+#     for p in p_list:
+#         p.join()
+#     print(f"final time:", datetime.now() - start)
+
+
+if __name__ == "__main__":  # 0:00:42.606745
+    """
+    Пулл процессов удобен для того чтобьі одну и ту же функцию вьізвать в нескольких потоках с разньіми аргументами.
+    Пулл работает ТОЛЬКО для тех функций, у которьіх есть аргумнтьі
+    """
+    start = datetime.now()
+    with multiprocessing.Pool(2) as pool:
+        pool.map(foo, (1, 2))
+    print(f"final time:", datetime.now() - start)
+
+
