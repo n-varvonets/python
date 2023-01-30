ee1bcc7ad82075adb43c82a47c8bf334877da327
n-varvonets <nickolay.varvonets@gmail.com>
2023-01-31 00:25:04 +0200

    passed queue datatype

--- test/lunchos/data_stucture/user-ze2jl8pf4m/3.queue.py
+++ test/lunchos/data_stucture/user-ze2jl8pf4m/3.queue.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, Optional
 
+
 @dataclass
 class Queue:
     """
@@ -27,7 +28,7 @@ class Queue:
     @dataclass
     class Node:
         element: Any
-        next_node: Optional[Queue.Node] = None
+        next_node: Optional = None
 
     head: Optional[Node] = None
     # length: Optional[int] = None - НЕПРАВИЛЬНЬІЙ ввод,
@@ -41,17 +42,67 @@ class Queue:
         :param element:
         :return:
         """
-        self.Node()
+        if not self.head:
+            self.head = self.Node(element)
+        else:
+            node = self.head
+            while node.next_node:
+                node = node.next_node
+
+            node.next_node = self.Node(element)
 
     def pop(self) -> Any:
         """
         Вернуть первьій елемент из очереди
         :return:
         """
+        try:
+            element = self.head.element
+        except AttributeError:
+            raise AttributeError('Нельзя взять аттрибут из пустой очереди')
+        self.head = self.head.next_node
+        return element
+
     @property
     def is_empty(self) -> bool:
         """
         property - потмоу никаких параметров он не принимает
         и по сути является скорее свойством, чем методом
-        :return:
+        Если головьі не будет, то будет  None, а None для bool - єто false,
+        а по лгике - наоборот: если список пуст, то нужно возращать true
         """
+        return not bool(self.head)
+
+    def show_result_print(self):
+        list_els = []
+        first_or_working_node = self.head
+        while first_or_working_node.next_node:
+            list_els.append(first_or_working_node.element)
+            first_or_working_node = first_or_working_node.next_node
+        list_els.append(first_or_working_node.element)
+        print(list_els)
+
+
+def main():
+    queue = Queue()
+    queue.add(123)
+    queue.add("my_string")
+    queue.add({"my_key": 4444})
+    queue.add(["my_list_string", 666666])
+    queue.show_result_print()
+
+    first_element = queue.pop()
+    print(f'\nfirst element = {first_element}and now aur list looks like')
+    queue.show_result_print()
+
+    # проврка обработки исклчения пустой очереди
+    queue.pop()
+    queue.pop()
+    queue.pop()
+    # queue.pop()  # AttributeError: Нельзя взять аттрибут из пустой очереди
+
+    print(queue.is_empty)  # -> True
+
+
+if __name__ == "__main__":
+    main()
