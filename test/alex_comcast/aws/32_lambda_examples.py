99308779d87281d19c9e0ea9d776fad666176bd9
n-varvonets <nickolay.varvonets@gmail.com>
2023-02-12 21:28:35 +0200

    passed API Gateway with Lamda func

--- test/alex_comcast/aws/32_lambda_examples.py
+++ test/alex_comcast/aws/32_lambda_examples.py
@@ -2,8 +2,7 @@
 # https://boto3.amazonaws.com/v1/documentation/api/latest/index.html
 # As an example there you can see 2 lamda func
 
-import boto3, json
-
+import boto3,json
 
 def lambda_handler(event, context):
     """Lambda func to list S3 Buckets"""
@@ -11,10 +10,9 @@ def lambda_handler(event, context):
     myS3 = boto3.client('s3')
     try:
         result = myS3.list_buckets()
-        print(f"result {result}")
         output = ""
         for bucket in result["Buckets"]:
-            output = output + bucket["name"] + "<br>"
+            output = output + bucket["Name"] + "<br>"
         return{
             'statusCode': 200,
             'body': json.dumps("<h1><font color=green>S3 Buckets List:<font><br><br>" + output)
@@ -32,28 +30,27 @@ import boto3, os, json, time
 
 AWS_DEFAULT_REGION = "ca-central-1"
 os.environ['AWS_DEFAULT_REGION'] = AWS_DEFAULT_REGION
-bucket_name = 'lambda.created.me - ' + str(time.time())
 
 
-def lambda_handler_(event, context):
-    """Lambda func to  create S3 Buckets"""
-    myS3 = boto3.client('s3')
-    try:
-        result = myS3.create_bucket(
-            Bucket=bucket_name,
-            CreateBucketConfiguration={
-                'LocationConstraint': AWS_DEFAULT_REGION
-            }
-        )
-        return{
-            'statusCode': 200,
-            'body': json.dumps("<h1><font color=green>S3 Created Successfully:</font><br><br>" )
-        }
-    except:
-        return{
-            'statusCode': 400,
-            'body': json.dumps("<h1><font color=red>my custom Error!</font><br><br>")
-        }
+# def lambda_handler(event, context):
+#     """Lambda func to  create S3 Buckets"""
+#     bucket_name = 'lambda.created.me-' + str(time.time())
+#     myS3 = boto3.resource('s3')  # be aware, here not client
+#     try:
+#         result = myS3.create_bucket(
+#             Bucket=bucket_name,
+#             CreateBucketConfiguration={'LocationConstraint': AWS_DEFAULT_REGION}
+#         )
+#         return{
+#             'statusCode': 200,
+#             'body': json.dumps("<h1><font color=green>S3 Created Successfully:</font><br><br>" )
+#         }
+#     except Exception as err:
+#         # print(err)
+#         return{
+#             'statusCode': 400,
+#             'body': json.dumps("<h1><font color=red>my custom Error!</font><br><br>", err)
+#         }
 
 
 
