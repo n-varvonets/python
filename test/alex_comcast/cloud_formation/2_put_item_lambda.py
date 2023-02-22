9bf75418a7f8fe1af27e3da24cd2ed5dc645b9be
n-varvonets <nickolay.varvonets@gmail.com>
2023-02-22 14:25:16 +0200

    refactor code for create method by resource

--- test/alex_comcast/cloud_formation/2_put_item_lambda.py
+++ test/alex_comcast/cloud_formation/2_put_item_lambda.py
@@ -3,35 +3,18 @@ import json
 from datetime import datetime
 import uuid
 
+GET_RAW_PATH = ""
+POST_RAW_PATH = "/test/create-update_new-user"
 
-def update_user(user_item):
-    # print('user_item=', user_item, type(user_item)) #
 
-    # Define the AWS access key ID and secret access key for the IAM user
-    aws_access_key_id = 'AKIASBKR2UWDIH5HHM4W'
-    aws_secret_access_key = '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'
-
-    # Create a session with the IAM user credentials
-    session = boto3.Session(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-    )
-    dynamodb = session.resource('dynamodb')  # resource лучге тем,
-    # что не надо указьівать каждтій раз тип данньіх с значению как {'S': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-
-    table_name = 'new_users'
-    table = dynamodb.Table(table_name)
+def create_new_user(table, user_item):
 
     # gathering params
-    # todo реализовать логику, если метод put, то не трограем дату добавления
-    if True:  # если create user -  удием в усвловие
-        added_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
     user_id = str(uuid.uuid4())
 
     new_item = {
         'ID': user_id,
-        'added': added_date,
+        'added': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'updated': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'ITEM': user_item
     }
@@ -39,20 +22,49 @@ def update_user(user_item):
     response = table.put_item(
         Item=new_item
     )
+    # make proper response
+    response = {
+        "entity_id": user_id,
+        "name_user": user_item['name'],
+        "status_in_db": "was successfully created"
+    }
 
     return response
 
 
 def lambda_handler(event, context):
+    print('event=', event)
 
-    # get params
-    raw_body_string = event["body"]
-    body_string = raw_body_string.replace("\n", "").replace(" ", "").replace('\\"', '\"').replace("'", "\"")
-    user_item = json.loads(body_string)
+    try:
+        # Define the AWS access key ID and secret access key for the IAM user
+        aws_access_key_id = 'AKIASBKR2UWDIH5HHM4W'
+        aws_secret_access_key = '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'
 
-    response = update_user(user_item)
+        # Create a session with the IAM user credentials and connect to our table in dynamoDB
+        session = boto3.Session(
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
+        dynamodb = session.resource('dynamodb')  # resource лучше чем client(т.к. не нужно указьівать тип значения)
+        table_name = 'new_users'
+        table = dynamodb.Table(table_name)
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response)
-    }
+        # make clear dict from string
+        raw_body_string = event["body"]
+        body_string = raw_body_string.replace("\n", "").replace(" ", "").replace('\\"', '\"').replace("'", "\"")
+        user_item = json.loads(body_string)
+
+        response = ""
+        if event['rawPath'] == POST_RAW_PATH:
+            response = create_new_user(table, user_item)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
+    except Exception as err:
+        print(err)
+        return {
+            'statusCode': 400,
+            'body': json.dumps("<h1><font color=red>my custom Error!</font><br><br>")
+        }
